3b09f7a6a223013f0920d8538e3426d3963f1026
wy0705 <2652918156@qq.com>
2022-03-30 21:55:15 +0800

    222

--- src/aaa/Test.java
+++ src/aaa/Test.java
@@ -1,8 +1,6 @@
 package aaa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 class Numb{
     ArrayList<Integer> arrayList=new ArrayList<Integer>();
@@ -40,6 +38,55 @@ public class Test {
         }
         for (int i = 0; i < n; i++) {
             System.out.println(numb.numb(nums[i]));
+        }
+    }
+}
+class Solution9 {
+    int[] dx = { 1, -1, 0, 0 };
+    int[] dy = { 0, 0, 1, -1 };
+    public void solve(char[][] board) {
+        int n = board.length;
+        if (n == 0) {
+            return;
+        }
+        int m = board[0].length;
+        Queue<int[]> queue = new LinkedList<int[]>();
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') {
+                queue.offer(new int[] { i, 0 });
+            }
+            if (board[i][m - 1] == 'O') {
+                queue.offer(new int[] { i, m - 1 });
+            }
+        }
+        for (int i = 1; i < m - 1; i++) {
+            if (board[0][i] == 'O') {
+                queue.offer(new int[] { 0, i });
+            }
+            if (board[n - 1][i] == 'O') {
+                queue.offer(new int[] { n - 1, i });
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int x = cell[0], y = cell[1];
+            board[x][y] = 'A';
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (board[n - 1][i] == 'O') {
+                    continue;
+                }
+                queue.offer(new int[] { mx, my });
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
         }
     }
 }
